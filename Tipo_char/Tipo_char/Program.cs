0eeb9edae3602da29081646225cdecfc77c564eb
Roberto B <robertbg08@gmail.com>
2016-08-17 14:07:28 +0200

    Pedir una letra y ver si es vocal,consonante u otro símbolo

---
+++ Tipo_char/Tipo_char/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+/*	 Crear un programa que pida una letra al usuario y diga si se trata de una
+vocal.
+*/
+
+using System;
+
+namespace Tipo_char
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			char letra;
+			bool es_vocal;
+
+			Console.WriteLine("Introduce una letra \n");
+			letra = Convert.ToChar(Console.ReadLine());
+
+			Console.WriteLine("La letra es {0}", letra);
+
+			es_vocal = (letra == 'a') || (letra == 'e') || (letra == 'i') || (letra == 'o') || (letra == 'u');
+
+			if (es_vocal)
+				Console.WriteLine("Es una vocal");
+			else
+				Console.WriteLine("Es una consonante u otro simbolo");
+
+
+		/*	if (letra == 'a')
+				if (letra == 'e')
+					if (letra == 'i')
+						if (letra == 'o')
+							if (letra == 'u')
+								Console.WriteLine("Es una vocal");
+							else
+								Console.WriteLine("Es una consonante");
+
+		*/
+		}
+	}
+}
+
+
