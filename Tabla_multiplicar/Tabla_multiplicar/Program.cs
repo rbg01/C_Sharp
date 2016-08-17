843d52e1ffaaa4b5301b7052f4244cf710eee1da
Roberto B <robertbg08@gmail.com>
2016-08-17 14:03:54 +0200

    Introduce un número y ver su tabla de multiplicar

---
+++ Tabla_multiplicar/Tabla_multiplicar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tabla_multiplicar
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int multiplica, resultado;
+			int contador;
+
+
+			/*	1- Pedir dato de entrada */
+			Console.WriteLine("Introduce un número para ver su tabla de multiplicar");
+			multiplica = Convert.ToInt32(Console.ReadLine());
+
+			for (contador = 1; contador <= 10; contador++)
+			{
+				resultado = multiplica * contador;
+				Console.WriteLine("{0} X {1} = {2} \n", multiplica, contador, resultado);
+			}
+		}
+	}
+}
