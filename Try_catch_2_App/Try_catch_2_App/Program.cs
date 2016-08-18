9b008a428bd987a83b9800857798a5ddb5381d40
Roberto B <robertbg08@gmail.com>
2016-08-18 18:00:32 +0200

    Uso de try/catch

---
+++ Try_catch_2_App/Try_catch_2_App/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Try_catch_2_App
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int num_1, num_2, resultado;
+
+			try
+			{
+				Console.WriteLine("Introduzca el primer número: ");
+				num_1 = Convert.ToInt32(Console.ReadLine());
+
+				Console.WriteLine("Introduzca el segundo número: ");
+				num_2 = Convert.ToInt32(Console.ReadLine());
+
+				resultado = num_1 / num_2;
+				Console.WriteLine("{0} / {1} = {2}", num_1, num_2, resultado);
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("No es un número valido");
+			}
+			catch (DivideByZeroException)
+			{
+				Console.WriteLine("No se puede dividir por cero");
+			}
+	}
+	}
+}
