064a85e8b1e96de1daabe71b9eb4b0709fd4ef3a
Roberto B <robertbg08@gmail.com>
2016-08-18 18:00:05 +0200

    Uso de try/catch

---
+++ Try_Catch_App/Try_Catch_App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Try_Catch_App
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int num_1, num_2, resultado;
+
+			try
+			{
+				Console.WriteLine("Introduzca el primer número");
+				num_1 = Convert.ToInt32(Console.ReadLine());
+
+				Console.WriteLine("Introduzca el segundo número");
+				num_2 = Convert.ToInt32(Console.ReadLine());
+
+				resultado = num_1 / num_2;
+
+				Console.WriteLine("Su división es {0}", resultado);
+			}
+			catch (Exception error_encontrado)
+			{
+				Console.WriteLine("Ha habído un error {0}", error_encontrado.Message);
+
+			}
+		}
+	}
+}
---
+++ Try_Catch_App/Try_Catch_App/version_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Try_Catch_App
+{
+	public class version_2
+	{
+		public version_2()
+		{
+			int numero1, numero2, resultado;
+			try
+			{
+				Console.WriteLine("Introduzca el primer numero");
+				numero1 = Convert.ToInt32(Console.ReadLine());
+				numero2 = Convert.ToInt32(Console.ReadLine());
+				resultado = numero1 / numero2;
+				Console.WriteLine("Su división es: {0}", resultado);
+
+			}
+			catch (FormatException)
+
+			{
+				Console.WriteLine("No es un número válido");
+
+			}
+			catch (DivideByZeroException)
+			{
+				Console.WriteLine("No se puede dividir entre cero");
+
+			}
+		}
+	}
+}
